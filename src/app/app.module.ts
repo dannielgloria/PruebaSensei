b7a606da1ae6ea2c6fa5f480d8c8cf2b823acdbd
dannielgloria <danniel.gloria@gmail.com>
2022-02-16 14:17:42 -0600

    Component modules are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarLogComponent } from './components/navbar-log/navbar-log.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PortalGunComponent } from './pages/portal-gun/portal-gun.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarLogComponent,
+    HomeComponent,
+    PortalGunComponent
   ],
   imports: [
     BrowserModule,
