6b259a8b5fd821f9deb95ab8fa0fa31184a2ac42
dannielgloria <danniel.gloria@gmail.com>
2022-02-16 21:19:10 -0600

    To develop on Heroku

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/PruebaDev'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/PruebaDev/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
